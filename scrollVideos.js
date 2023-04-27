ddf4082295f9b641fd1f1e7ca7babb92e7131938
Frejbo <frej.hellsten@gmail.com>
2023-04-27 15:17:15 +0200

    fungerar inte, försökt lägga canvas bakom

--- scrollVideos.js
+++ scrollVideos.js
@@ -35,15 +35,33 @@ const element_names = [[pizzaCanvas, "pizzavideo/img", pizza_frameCount, pizza_i
 
 
 
-
 window.addEventListener("scroll", () => {
+    
+    
     for (let i = 0; i < element_names.length; i++) {
         update_canvas(element_names[i][0], element_names[i][1], element_names[i][2], element_names[i][3], element_names[i][4])
+        break;
     }
 })
 
 function updateImage(index, img, canvas, path) {
+    let pizza_height =document.querySelector("#pizzasize").clientHeight;
+    let pizza_width =document.querySelector("#pizzasize").clientWidth;
+    // console.log(pizza_height);
+    document.querySelector(".image-container").style.height = pizza_height + "px";
+    // pizzaCanvas.height = pizza_height + "px";
+    
+    let scale_fac = pizza_height / pizza_img.height;
+    // console.log(pizza_width * scale_fac)
+    img.width = 640 * scale_fac;
+    // pizzaCanvas.width = 640 * scale_fac;
+
+
+
     img.src = path + index.toString().padStart(4, "0") + ".jpg";
+    console.log(img.width);
+    img.width = 500;
+    console.log(img.width);
     canvas.getContext("2d").drawImage(img, 0, 0)
     // document.querySelector("#pizza").style.backgroundImage = "url("+img.src+")";//"url("+canvas.toDataURL()+")";
 }
@@ -51,7 +69,8 @@ function updateImage(index, img, canvas, path) {
 function update_canvas(canvas, path, frameCount, img, elementname) {
     // parentElement är en image-container som justerar lite cropping i vissa aspect ratios. Gör beräkningarna utifrån den istället för canvas, då hela canvas height kanske inte syns.
     var scrollTop = -document.querySelector(elementname).parentElement.getBoundingClientRect().bottom + (window.innerHeight)//+250)
-    var maxScroll = (window.innerHeight - document.querySelector(elementname).parentElement.clientHeight)// * .8 // the length of which it should scroll within.
+    // var maxScroll = (window.innerHeight - document.querySelector(elementname).parentElement.clientHeight)// * .8 // the length of which it should scroll within.
+    var maxScroll = (window.innerHeight - document.querySelector(elementname).clientHeight)// * .8 // the length of which it should scroll within.
     var scrollFraction = (scrollTop / maxScroll)
     // console.log("top", scrollTop, "max", maxScroll, "frac", scrollFraction)
     var frameIndex = Math.min(frameCount - 1, Math.floor(scrollFraction * frameCount))
